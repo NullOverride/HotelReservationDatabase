460abb3145014ed5dc1567a79b45e6fdb72e1790
NullOverride <imdafatguy@gmail.com>
2016-12-01 12:52:23 -0800

    Tables displays properly, ugly looking though

--- src/HotelGUI.java
+++ src/HotelGUI.java
@@ -8,15 +8,22 @@ import com.mysql.jdbc.Statement;
 
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
-import javax.swing.JCheckBox;
 import javax.sql.DataSource;
 import javax.swing.JButton;
+import javax.swing.JTable;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.GridLayout;
+import javax.swing.JScrollPane;
 
 public class HotelGUI {
 
 	private JFrame frmHotelReservation;
 
-	private static Connection connection =  null; 
+	private static Connection connection; 
+	private ResultSet rs;
+	private JTable table;
+	ListTableModel model;
 
 	/**
 	 * Launch the application.
@@ -25,7 +32,8 @@ public class HotelGUI {
 		DataSource ds = DataSourceFactory.getMySQLDataSource();     
 
 		try {
-			connection = ds.getConnection(); 
+			connection = ds.getConnection();
+			
 		} catch (SQLException e) {
 			System.out.println("Connection Failed! Check output console");
 			e.printStackTrace();
@@ -34,10 +42,10 @@ public class HotelGUI {
 
 		if (connection != null) {
 			System.out.println("You made it, take control your database now!");
+			
 		} else {
 			System.out.println("Failed to make connection!");
 		}
-
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -48,6 +56,8 @@ public class HotelGUI {
 				}
 			}
 		});
+
+		
 	}
 
 	/**
@@ -63,13 +73,11 @@ public class HotelGUI {
 	 * @throws SQLException 
 	 */
 	private void initialize() throws SQLException {
-		ResultSet rs = null;
+
 		
 	
 		Statement stmt = (Statement) connection.createStatement();
 		
-		rs = stmt.executeQuery("SELECT * FROM Room");
-		printResultSet(rs);
 	
 		frmHotelReservation = new JFrame();
 		frmHotelReservation.setTitle("Hotel Reservation");
@@ -81,6 +89,41 @@ public class HotelGUI {
 
 		JPanel panel = new JPanel();
 		tabbedPane.addTab("Customer", null, panel, null);
+		
+		JButton btnGetRooms = new JButton("Get rooms");
+		btnGetRooms.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					 rs = stmt.executeQuery("SELECT * FROM Room");
+					 model = ListTableModel.createModelFromResultSet(rs);
+					 table.setModel(model);
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		panel.setLayout(new GridLayout(0, 2, 0, 0));
+		panel.add(btnGetRooms);
+		
+		JButton btnNewButton = new JButton("Get rIDs");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					 rs = stmt.executeQuery("SELECT rID FROM Room");
+					 model = ListTableModel.createModelFromResultSet(rs);
+					 table.setModel(model);
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		panel.add(btnNewButton);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
 
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Manager", null, panel_1, null);
