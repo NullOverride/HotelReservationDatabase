0eb2e482c427f8f106254b372aa2f951dfd1236c
Jeffrey <jephroz@gmail.com>
2016-12-05 21:18:40 -0800

    delete props.db
    not needed anymore?

--- src/DataSourceFactory.java
+++ src/DataSourceFactory.java
@@ -9,20 +9,10 @@ import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 public class DataSourceFactory {
     public static DataSource getMySQLDataSource(String uName, String pw) {
         
-    	Properties props = new Properties();
-        FileInputStream fis = null;
         MysqlDataSource mysqlDS = null;
-        try {
-            fis = new FileInputStream("db.properties");
-            props.load(fis);
             mysqlDS = new MysqlDataSource();
-            mysqlDS.setURL(props.getProperty("MYSQL_DB_URL"));
             mysqlDS.setUser(uName);
             mysqlDS.setPassword(pw);
-        } catch (IOException e) {
-        	System.out.println("db.properties is not found");
-            e.printStackTrace();
-        }
         return mysqlDS;
       
     }
